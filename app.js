38a3d8bf6f9bbf6b4f8278a405f8f17360693c81
hmshujaatali <81732578+hmshujaatali@users.noreply.github.com>
2021-08-22 02:06:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+var data = [
+    {
+        brand: "Samsung 1",
+        name: "Samsung Galaxy A32",
+        price: 37000,
+        url: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a32-5g.jpg"
+    },
+    {
+        brand: "Samsung 2",
+        name: "Iphone Galaxy A32",
+        price: 37000,
+        url: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a32-5g.jpg"
+    },
+    {
+        brand: "Samsung 3",
+        name: "Samsung Galaxy A32",
+        price: 37000,
+        url: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a32-5g.jpg"
+    }
+];
+
+
+// let filterData = data.filter(val => val.name.startsWith("i"))
+
+// console.log(filterData)
+
+
+// var products = document.getElementById("products");
+// for (var i = 0; i < data.length; i++) {
+//     products.innerHTML += `
+//     <div class="card" style="width: 18rem;">
+//   <img src="${data[i].url}" class="card-img-top" alt="...">
+//   <div class="card-body">
+//     <h5 class="card-title">${data[i].brand}</h5>
+//     <p class="card-text">${data[i].price}</p>
+//     <a href="#" onclick="addToCart(${i})" class="btn btn-primary">Add to cart</a>
+//   </div>
+// </div>
+//     `
+// }
+
+// let addToCart = (i) => {
+//     console.log(data[i])
+// }
+
+let a={
+    {
+    name:"ali",
+    id:"111",
+
+}}
